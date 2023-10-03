86a60880171633bec2f29ab94a24858aeb7ffd1e
yamazakimina <130649486+yamazakimina@users.noreply.github.com>
2023-10-03 18:50:28 +0900

    test

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,16 +2,12 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-/**
- * @property int $id
- * @property string $name
- * @property string $email
- */
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\FanCertificatesController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\VtuberController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,29 +16,3 @@ use App\Http\Controllers\VtuberController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    // fan certificates
-    Route::get('/fan-certificates', [FanCertificatesController::class, 'index'])->name('fan-certificates.index');
-    Route::get('/fan-certificates/create/{vtuberId}', [FanCertificatesController::class, 'create'])->name('fan-certificates.create');
-    Route::post('/fan-certificates/create/{vtuberId}', [FanCertificatesController::class, 'store'])->name('fan-certificates.store');
-    Route::get('/fan-certificates/edit/{id}', [FanCertificatesController::class, 'edit'])->name('fan-certificates.edit');
-    Route::put('/fan-certificates/edit/{id}', [FanCertificatesController::class, 'update'])->name('fan-certificates.update');
-    Route::get('/fan-certificates/{id}', [FanCertificatesController::class, 'show'])->name('fan-certificates.show');
-    Route::get('/fan-certificates/show/{id}', [FanCertificatesController::class, 'show'])->name('fan-certificates.show');
-
-    // profile
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/vtubers', [VtuberController::class, 'index'])->name('vtuber.index');
-Route::get('/dashboard', [VtuberController::class, 'index'])->name('dashboard');
-Route::get('/vtubers/{vtuberId}', [VtuberController::class, 'show'])->name('vtuber.show');
-
-require __DIR__.'/auth.php';
